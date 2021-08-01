3abc56eb80c1f72ace2e5127ba59dc698a4b78ce
Erik Overdahl <erik.overdahl@gmail.com>
2021-08-01 13:49:45 -0400

    Help strings for cli subcommands

--- huberman_lab_transcripts/cli.py
+++ huberman_lab_transcripts/cli.py
@@ -15,6 +15,7 @@ app = typer.Typer(name="huberman-transcripts")
 @app.command()
 def download(video_or_playlist_ids: List[str] = typer.Argument(..., help="One or more youtube video ids or playlist ids"),
              data_dir: str = typer.Option('./data', help="Directory into which to download data")):
+    """A wrapper around youtube-dl for downloading video data"""
     downloader = PodcastInfoDownloader(Path(data_dir))
     downloader.download(video_or_playlist_ids)
 
@@ -23,6 +24,7 @@ def download(video_or_playlist_ids: List[str] = typer.Argument(..., help="One or
 def generate(video_ids: List[str] = typer.Argument(None, help="The youtube video ids for which to generate markdown files. If empty, generate files for all ids in [DATA_DIR]"),
              data_dir: str = typer.Option('./data', help="Directory of raw video data"),
              target_dir: str = typer.Option('./site/content/posts', help="Directory into which to place generated markdown files for static site generator")):
+    """Generate markdown files for Pelican"""
     extractor = PodcastExtractor(Path(data_dir).expanduser().absolute())
     podcasts = extractor.extract_all(video_ids)
     target_dir = Path(target_dir).expanduser().absolute()
